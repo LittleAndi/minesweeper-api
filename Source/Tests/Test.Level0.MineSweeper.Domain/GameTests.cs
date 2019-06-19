181c6f38021b2c69f18bc50412f3762e82a3f5b9
Lars Nyström <nystrom.lasse@gmail.com>
2019-06-19 23:38:51 +0200

    Add game test

--- Source/Tests/Test.Level0.MineSweeper.Domain/GameTests.cs
+++ Source/Tests/Test.Level0.MineSweeper.Domain/GameTests.cs
@@ -1,3 +1,4 @@
+using MineSweeper.Domain;
 using Xunit;
 
 namespace Test.Level0.MineSweeper.Domain
@@ -5,6 +6,12 @@ namespace Test.Level0.MineSweeper.Domain
     [Trait("Category", "L0")]
     public class GameTests
     {
-        
+        [Fact]
+        public void ShoudGenerateNewGameWithATenByTenBoard()
+        {
+            var game = new Game(10, 10, 10);
+            Assert.NotNull(game);
+            Assert.NotNull(game.Board);
+        }
     }
 }
