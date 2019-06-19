785c861afbd06aa09bdada469094750525b9b674
Lars Nyström <nystrom.lasse@gmail.com>
2019-06-19 23:38:36 +0200

    Add a board to the game

--- Source/MineSweeper.Domain/Game.cs
+++ Source/MineSweeper.Domain/Game.cs
@@ -5,5 +5,18 @@ namespace MineSweeper.Domain
     public class Game
     {
         public Guid GameId { get; private set; }
+        public int BoardSizeX { get; set; }
+        public int BoardSizeY { get; set; }
+        public int Mines { get; set; }
+        public Guid BoardId { get; private set; }
+
+        public Board Board { get; set; }
+
+        public Game(int boardSizeX, int boardSizeY, int mines)
+        {
+            BoardSizeX = boardSizeX;
+            BoardSizeY = boardSizeY;
+            Mines = mines;
+        }
     }
 }
