5aa14300d31cadc6225d1957c611de3f645f5fff
Lars Nyström <nystrom.lasse@gmail.com>
2019-06-24 20:31:10 +0200

    Create empty layout

--- Source/MineSweeper.Domain/Board.cs
+++ Source/MineSweeper.Domain/Board.cs
@@ -4,17 +4,30 @@ namespace MineSweeper.Domain
 {
     public class Board
     {
+        private Random random;
         private int boardSizeX;
         private int boardSizeY;
         private int mines;
+        private int[,] boardLayout;
 
         public int MineCount => int.MinValue;
 
         public Board(int boardSizeX, int boardSizeY, int mines)
         {
+            this.random = new Random(DateTime.Now.Millisecond);
+
             this.boardSizeX = boardSizeX;
             this.boardSizeY = boardSizeY;
             this.mines = mines;
+
+            boardLayout = CreateLayout();
+        }
+
+        private int[,] CreateLayout()
+        {
+            var layout = new int[this.boardSizeX, this.boardSizeY];
+
+            return layout;
         }
     }
 }
