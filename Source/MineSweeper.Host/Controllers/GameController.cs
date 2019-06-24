80d4357b803a280c74497495694fcbe1f7e1f8f3
Lars Nyström <nystrom.lasse@gmail.com>
2019-06-24 22:47:03 +0200

    Add GameController

---
+++ Source/MineSweeper.Host/Controllers/GameController.cs
@@ -0,0 +1,13 @@
+namespace MineSweeper.Host.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        [HttpPost]
+        public void Post()
+        {
+            
+        }
+    }
+}
