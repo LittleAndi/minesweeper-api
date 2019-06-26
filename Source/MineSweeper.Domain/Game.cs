d2e535e2ad9703b222456993a9dbd5e410a070f7
Lars Nyström <nystrom.lasse@gmail.com>
2019-06-26 22:46:29 +0200

    Generate new GameId

--- Source/MineSweeper.Domain/Game.cs
+++ Source/MineSweeper.Domain/Game.cs
@@ -15,6 +15,7 @@ namespace MineSweeper.Domain
         public Game(int boardSizeX, int boardSizeY, int mines)
         {
             Board = new Board(boardSizeX, boardSizeY, mines);
+            GameId = new Guid();
         }
     }
 }
