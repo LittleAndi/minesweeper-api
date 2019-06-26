164d312fdf6fe9eab8bf1b2f39fe0f05dda311b9
Lars Nyström <nystrom.lasse@gmail.com>
2019-06-26 23:15:14 +0200

    Add GameDataContracts

--- Source/MineSweeper.Host/DataContracts/GameDataContract.cs
+++ Source/MineSweeper.Host/DataContracts/GameDataContract.cs
@@ -1,7 +1,12 @@
+using System;
+
 namespace MineSweeper.Host.DataContracts
 {
     public class GameDataContract
     {
-        
+        public Guid GameId { get; set; }
+        public int BoardSizeX { get; }
+        public int BoardSizeY { get; }
+        public int Mines { get; }
     }
 }
