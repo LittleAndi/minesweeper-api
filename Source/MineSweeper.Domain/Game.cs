74963aa1eecc08f48e8d281d9a939596e00d31a2
Lars Nyström <nystrom.lasse@gmail.com>
2019-06-26 22:50:26 +0200

    Fix GameId generation

--- Source/MineSweeper.Domain/Game.cs
+++ Source/MineSweeper.Domain/Game.cs
@@ -10,7 +10,7 @@ namespace MineSweeper.Domain
         public Game(int boardSizeX, int boardSizeY, int mines)
         {
             Board = new Board(boardSizeX, boardSizeY, mines);
-            GameId = new Guid();
+            GameId = Guid.NewGuid();
         }
     }
 }
