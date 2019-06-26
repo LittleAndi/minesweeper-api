74361da56e0e2ae5067a0d2818c9b104769256b1
Lars Nyström <nystrom.lasse@gmail.com>
2019-06-26 21:59:44 +0200

    Add test project for application

---
+++ Source/Tests/Test.Level0.MineSweeper.Application/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Test.Level0.MineSweeper.Application
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
