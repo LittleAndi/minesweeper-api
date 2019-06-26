a8b97ff0c45b039968cb782abec2d5585a69675e
Lars Nyström <nystrom.lasse@gmail.com>
2019-06-26 22:01:26 +0200

    Add test for host project

---
+++ Source/Tests/Test.Level0.MineSweeper.Host/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Test.Level0.MineSweeper.Host
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
