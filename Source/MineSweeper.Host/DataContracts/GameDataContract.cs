f55e90d9e568dd4a9f7345563cde085d0cf66dfe
Lars Nyström <nystrom.lasse@gmail.com>
2020-08-08 00:44:48 +0200

    Fix failing tests due to missing sets

--- Source/MineSweeper.Host/DataContracts/GameDataContract.cs
+++ Source/MineSweeper.Host/DataContracts/GameDataContract.cs
@@ -5,8 +5,8 @@ namespace MineSweeper.Host.DataContracts
     public class GameDataContract
     {
         public Guid GameId { get; set; }
-        public int BoardSizeX { get; }
-        public int BoardSizeY { get; }
-        public int Mines { get; }
+        public int BoardSizeX { get; set; }
+        public int BoardSizeY { get; set; }
+        public int Mines { get; set; }
     }
 }
