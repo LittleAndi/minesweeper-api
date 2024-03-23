eb36a8d7bbda7140dbc8984ee6873c1a2886a8ad
Lars Nyström <nystrom.lasse@gmail.com>
2024-03-23 22:37:26 +0100

    Add square reveal tests for first four lines

--- Source/Tests/Test.Level0.Application/BoardTests.cs
+++ Source/Tests/Test.Level0.Application/BoardTests.cs
@@ -16,7 +16,7 @@ public class BoardTests
     }
 
     [Theory]
-    // The first 10 squares should be mines
+    // The first diagonal 10 squares should be mines
     [InlineData(0, 0, true, 1)]
     [InlineData(1, 1, true, 2)]
     [InlineData(2, 2, true, 2)]
@@ -28,21 +28,54 @@ public class BoardTests
     [InlineData(8, 8, true, 2)]
     [InlineData(9, 9, true, 1)]
 
-    // Line 1
+    // Line 0
     [InlineData(0, 1, false, 2)]
     [InlineData(0, 2, false, 1)]
     [InlineData(0, 3, false, 0)]
     [InlineData(0, 4, false, 0)]
-    [InlineData(0, 5, false, 0)]
-    [InlineData(0, 6, false, 0)]
-    [InlineData(0, 7, false, 0)]
-    [InlineData(0, 8, false, 0)]
-    [InlineData(0, 9, false, 0)]
+    [InlineData(0, 5, false, 1)]
+    [InlineData(0, 6, false, 2)]
+    [InlineData(0, 7, false, 3)]
+    [InlineData(0, 8, false, 2)]
+    [InlineData(0, 9, false, 1)]
+
+    // Line 1
+    [InlineData(1, 0, false, 2)]
+    [InlineData(1, 2, false, 2)]
+    [InlineData(1, 3, false, 1)]
+    [InlineData(1, 4, false, 0)]
+    [InlineData(1, 5, false, 2)]
+    [InlineData(1, 6, true, 3)]
+    [InlineData(1, 7, true, 5)]
+    [InlineData(1, 8, true, 3)]
+    [InlineData(1, 9, false, 2)]
+
+    // Line 2
+    [InlineData(2, 0, false, 1)]
+    [InlineData(2, 1, false, 2)]
+    [InlineData(2, 3, false, 2)]
+    [InlineData(2, 4, false, 1)]
+    [InlineData(2, 5, false, 3)]
+    [InlineData(2, 6, true, 5)]
+    [InlineData(2, 7, true, 8)]
+    [InlineData(2, 8, true, 5)]
+    [InlineData(2, 9, false, 3)]
+
+    // Line 3
+    [InlineData(3, 0, false, 0)]
+    [InlineData(3, 1, false, 1)]
+    [InlineData(3, 2, false, 2)]
+    [InlineData(3, 4, false, 2)]
+    [InlineData(3, 5, false, 3)]
+    [InlineData(3, 6, true, 3)]
+    [InlineData(3, 7, true, 5)]
+    [InlineData(3, 8, true, 3)]
+    [InlineData(3, 9, false, 2)]
     public void ShouldRevealSquare(int x, int y, bool isMine, int adjacentMines)
     {
         var boardSizeX = 10;
         var boardSizeY = 10;
-        var mines = 10;
+        var mines = 19;
         var board = new Board(boardSizeX, boardSizeY, mines, new NonRandomMinePlacementStrategy());
         var squareInfo = board.RevealSquare(x, y);
         squareInfo.ShouldNotBeNull();
--- Source/Tests/Test.Level0.Application/Common.cs
+++ Source/Tests/Test.Level0.Application/Common.cs
@@ -16,16 +16,20 @@ internal class NonRandomMinePlacementStrategy : IRandomGenerator
 
     // These "random" numbers should place mines in a diagonal line from top-left to bottom-right
     /*
-    X 0 0 0 0 0 0 0 0 0
-    0 X 0 0 0 0 X X X 0
-    0 0 X 0 0 0 X X X 0
-    0 0 0 X 0 0 X X X 0
-    0 0 0 0 X 0 0 0 0 0
-    0 0 0 0 0 X 0 0 0 0
-    0 0 0 0 0 0 X 0 0 0
-    0 0 0 0 0 0 0 X 0 0
-    0 0 0 0 0 0 0 0 X 0
-    0 0 0 0 0 0 0 0 0 X
+
+        0 1 2 3 4 5 6 7 8 9
+        -------------------
+    0 | X 0 0 0 0 0 0 0 0 0
+    1 | 0 X 0 0 0 0 X X X 0
+    2 | 0 0 X 0 0 0 X X X 0
+    3 | 0 0 0 X 0 0 X X X 0
+    4 | 0 0 0 0 X 0 0 0 0 0
+    5 | 0 0 0 0 0 X 0 0 0 0
+    6 | 0 0 0 0 0 0 X 0 0 0
+    7 | 0 0 0 0 0 0 0 X 0 0
+    8 | 0 0 0 0 0 0 0 0 X 0
+    9 | 0 0 0 0 0 0 0 0 0 X
+
     */
     private readonly int[] randomNumbers = [
         0,0,
