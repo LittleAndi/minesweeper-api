bede905ca34ec3193de22e93cd491f695d3a046a
Lars Nyström <nystrom.lasse@gmail.com>
2024-03-31 16:58:02 +0200

    Fix dependency

--- Source/Tests/Test.Level0.Host/GlobalUsings.cs
+++ Source/Tests/Test.Level0.Host/GlobalUsings.cs
@@ -1,6 +1,7 @@
 global using Application;
 global using AutoMapper;
 global using Host;
+global using Host.Endpoints;
 global using Shouldly;
 global using System;
 global using System.Threading.Tasks;
