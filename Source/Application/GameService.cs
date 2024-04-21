17551ea72e83da9cc44c5c5075322820118547cd
Lars Nyström <nystrom.lasse@gmail.com>
2024-04-21 03:10:51 +0200

    Add an appropriate end-of-game (win/loss)

--- Source/Application/GameService.cs
+++ Source/Application/GameService.cs
@@ -24,7 +24,26 @@ public class GameService(IGameCreationService gameCreationService) : IGameServic
                 throw new Exception("Game not found");
             }
             var square = game.Board.RevealSquare(x, y);
+
+            if (square.IsMine)
+            {
+                game.Status = GameStatus.Lost;
+            }
+            else if (game.Board.BoardSizeX * game.Board.BoardSizeY - game.Board.MineCount == game.Board.RevealedCount)
+            {
+                game.Status = GameStatus.Won;
+            }
+
             return square;
         });
+    }
+
+    public GameStatus GetGameStatus(Guid gameId)
+    {
+        if (!games.TryGetValue(gameId, out var game))
+        {
+            throw new Exception("Game not found");
+        }
+        return game.Status;
     }
 }
--- Source/Application/IGameService.cs
+++ Source/Application/IGameService.cs
@@ -7,4 +7,5 @@ public interface IGameService
 {
     Task<Game> StartGame(int boardSizeX, int boardSizeY, int mines);
     Task<SquareInfo> RevealSquare(Guid gameId, int x, int y);
+    GameStatus GetGameStatus(Guid gameId);
 }
